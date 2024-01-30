6e810162f8e141a9a324019a7160ab3db6b98c3a
Jérémie <jeremie.faussion@gmail.com>
2024-01-30 22:36:08 +0100

    [FEAT] Create climbing track card

---
+++ app/domain/TrackSchema.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+// Define the schema for a track
+export const TrackSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  date: z.string(),
+  imageUrl: z.string().optional(),
+  holdColor: z.string().optional(),
+  level: z.enum(['Unknown', 'Beginner', 'Easy', 'Intermediate', 'Advanced', 'Difficult', 'FuckingHard', 'Legendary']).default('Unknown'),
+  status: z.enum(['ToDo', 'InProgress', 'Done']).default('ToDo'),
+});
+
+export type Track = z.infer<typeof TrackSchema>;
+
+export const getColorForLevel = (level: string): string => {
+  const levelColorMapping = {
+    Unknown: 'gray-500',
+    Beginner: 'white-500',
+    Easy: 'green-500',
+    Intermediate: 'blue-500',
+    Advanced: 'pink-500',
+    Difficult: 'red-500',
+    FuckingHard: 'yellow-500',
+    Legendary: 'black-500',
+  };
+
+  return levelColorMapping[level] || 'gray-500'; // default color if level is not found
+};
+
+
+
