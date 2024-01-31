a8d2dbe13fa209bc1fb83eda4595b29ab6ab0d15
Jérémie <jeremie.faussion@gmail.com>
2024-01-31 23:56:17 +0100

    [FEAT] Order tracks by date desc

--- app/api/tracks.tsx
+++ app/api/tracks.tsx
@@ -3,7 +3,7 @@ import ClimbingTrackCard from "../components/climbingTrack-card";
 import { Track, TrackSchema } from "../domain/TrackSchema";
 
 export default async function Tracks(): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from climbing_track`;
+  const { rows } = await sql`SELECT * from climbing_track order by date desc`;
   let trackList: Track[] = rows.map((row: any) => TrackSchema.parse(row));
 
   return (
