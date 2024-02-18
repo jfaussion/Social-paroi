2698065f199e3205207c7cd7be49181ea462a543
Jérémie <jeremie.faussion@gmail.com>
2024-02-18 13:40:12 +0100

    [FEAT] Signin button providers

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,10 +4,13 @@ export default function LoginPage() {
 
   return (
     <main className="flex items-center justify-center md:h-screen">
-      <div>
-        <h1>Login</h1>
-        <SignInWithProviverButton provider="github"/>
-        <SignInWithProviverButton provider="google"/>
+      <div className="max-w-sm mx-auto my-8 p-6 bg-gray-900 rounded-lg shadow-md space-y-4">
+
+        <div className="bg-blue-500 h-20 w-20 rounded-full mx-auto">
+          {/* Image placeholder */}
+        </div>
+        <SignInWithProviverButton provider="github" providerName='GitHub' providerImage='./github-mark-white.svg' />
+        <SignInWithProviverButton provider="google" providerName='Google' providerImage='./google-icon.svg' />
       </div>
     </main>
   );
--- app/ui/SignInWithProviderButton.tsx
+++ app/ui/SignInWithProviderButton.tsx
@@ -3,12 +3,18 @@
 import { signIn } from "next-auth/react";
 
 export default function SignInWithProviverButton(
-  { provider }: Readonly<{ provider: string }>
+  { provider, providerName, providerImage }: Readonly<{ provider: string, providerName: string, providerImage: string }>
 ) {
 
   return (
     <div>
-      <button onClick={() => signIn(provider)}>Log in with { provider }</button>
+      <button
+        className="bg-gray-800 text-white rounded-lg px-4 py-2 flex items-center space-x-2 hover:bg-gray-700"
+        onClick={() => signIn(provider)}
+      >
+        <img src={providerImage} alt={`${providerName} Icon`} className="w-5 h-5" />
+        <span>Sign in with {providerName}</span>
+      </button>
     </div>
   );
 
