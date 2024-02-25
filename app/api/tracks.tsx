8bd8b3fcd5999b734f5166412f01cbba26baf07e
Jérémie <jeremie.faussion@gmail.com>
2024-02-25 23:22:17 +0100

    Refactor Tracks component to use TrackCard and useEffect

--- app/api/tracks.tsx
+++ app/api/tracks.tsx
@@ -1,16 +1,32 @@
-import { sql } from "@vercel/postgres";
-import ClimbingTrackCard from "../../components/climbingTrack-card";
-import { Track, TrackSchema } from "../../domain/TrackSchema";
+'useclient'
+import TrackCard from "../../components/TrackCard";
+import { Track } from "../../domain/TrackSchema";
+import { useEffect, useState } from "react";
+import { getAllTracksForUser } from "../lib/actions";
 
-export default async function Tracks(): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from tracks order by date desc`;
-  let trackList: Track[] = rows.map((row: any) => TrackSchema.parse(row));
+type TracksProps = {
+  userId: string;
+};
+
+const Tracks: React.FC<TracksProps> = ({ userId }) => {
+  const [trackList, setTrackList] = useState<Track[]>([]);
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      const tracks = await getAllTracksForUser(parseInt(userId));
+      setTrackList(tracks);
+    };
+
+    fetchTracks();
+  }, [userId]); // Re-fetch when userId changes
 
   return (
     <div className="space-y-4">
-        {(trackList).map((track) => (
-          <ClimbingTrackCard key={track.id} {...track} ></ClimbingTrackCard>
-        ))}
-      </div>
+      {trackList.map((track) => (
+        <TrackCard key={track.id} {...track} />
+      ))}
+    </div>
   );
-}
+};
+
+export default Tracks;
