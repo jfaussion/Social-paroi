b6bc2f3aeefb21ba46ac827604b10502cce0cbbe
Jérémie <jeremie.faussion@gmail.com>
2024-03-13 21:27:07 +0100

    Add URL query parameters for filtering tracks

--- components/TrackList.tsx
+++ components/TrackList.tsx
@@ -6,33 +6,68 @@ import { useFetchTracks } from "@/lib/featchTracksHook";
 import { CardPlaceHolder } from "./ui/CardPlacehorlder";
 import DifficultyFilter from "./DifficultyFilter";
 import ZoneFilter from "./ZoneFilter";
+import { useRouter, usePathname, useSearchParams } from 'next/navigation'
 
 type TracksProps = {
   userId: string;
 };
 
 const TrackList: React.FC<TracksProps> = ({ userId }) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
   const [trackList, setTrackList] = useState<Track[]>([]);
   const [selectedDifficulties, setSelectedDifficulties] = useState<string[]>([]);
   const [selectedZones, setSelectedZones] = useState<number[]>([]);
   const { fetchTracks, isLoading, error } = useFetchTracks();
+  const current = new URLSearchParams(Array.from(searchParams.entries())); // -> has to use this form
 
   useEffect(() => {
-    const getTracks = async () => {
-      const tracks = await fetchTracks(userId, selectedZones, selectedDifficulties);
+    const getTracks = async (zones: number[] | undefined, difficulties: string[] | undefined) => {
+      const tracks = await fetchTracks(userId, zones, difficulties);
       setTrackList(tracks);
     };
+    // Parse URL query parameters to get filter
+    const zones = searchParams.has('zones') ? searchParams.get('zones')?.split(',').map(Number) : [];
+    const difficulties = searchParams.has('difficulties') ? searchParams.get('difficulties')?.split(',') : [];
+    getTracks(zones, difficulties);
+  }, [userId, searchParams]);
 
-    getTracks();
-  }, [userId, selectedZones, selectedDifficulties]);
+  const updateFiltersInURL = (zones: any[], difficulties: any[]) => {
+    current.delete('zones');
+    current.delete('difficulties');
+    if (zones.length > 0) {
+      current.set('zones', zones.join(','));
+    }
+    if (difficulties.length > 0) {
+      current.set('difficulties', difficulties.join(','));
+    }
+
+    const search = current.toString();
+    const query = search ? `?${search}` : "";
+    router.push(`${pathname}${query}`);
+  };
+
+  const handleZoneChange = (selectedOptions: { value: any; }[]) => {
+    const zones = selectedOptions.map((option: { value: any; }) => option.value);
+    setSelectedZones(zones);
+    updateFiltersInURL(zones, selectedDifficulties);
+  };
+
+  const handleDifficultyChange = (selectedOptions: { value: any; }[]) => {
+    const difficulties = selectedOptions.map((option: { value: any; }) => option.value);
+    setSelectedDifficulties(difficulties);
+    updateFiltersInURL(selectedZones, difficulties);
+  };
+
 
   return (
     <div className="space-y-4 w-full max-w-3xl">
       <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 mt-2">
         <ZoneFilter selectedFilters={selectedZones}
-          onChange={(selectedOptions: any[]) => setSelectedZones(selectedOptions.map((option: { value: any; }) => option.value))} />
+          onChange={handleZoneChange} />
         <DifficultyFilter selectedFilters={selectedDifficulties}
-          onChange={(selectedOptions: any[]) => setSelectedDifficulties(selectedOptions.map((option: { value: any; }) => option.value))} />
+          onChange={handleDifficultyChange} />
       </div>
       {isLoading ? (
         <>
