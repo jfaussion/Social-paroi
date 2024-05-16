30c01fe164c83bb34cd44d430d0303f7c320eaf8
Jérémie <jeremie.faussion@gmail.com>
2024-05-16 20:59:25 +0200

    feat: add footer links to Drawer component

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -64,6 +64,8 @@ const Drawer = () => {
               </>
             )}
           </ul>
+
+          {/* Footer links */}
           <div className="mt-auto p-5 flex flex-wrap justify-start text-xs text-blue-500 font-mono space-x-1">
             <Link className="hover:underline" href="/privacy">Privacy policy</Link><span>|</span>
             <Link className="hover:underline" href="https://github.com/jfaussion/Social-paroi">Contribute</Link><span>|</span>
