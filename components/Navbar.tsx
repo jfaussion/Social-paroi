756f97c4e242b615747c7513081cae4e715c32b2
Jérémie <64860341+jfaussion@users.noreply.github.com>
2024-05-16 21:02:03 +0200

    Feature/drawer menu (#29)
    * Init drawer menu
    
    * [FIX] drawer z-index
    
    * Refactor Drawer component to improve menu navigation and add back button support
    
    * Align close button to top corner right
    
    * Add drawer footer with utility links
    
    * feat: add footer links to Drawer component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { auth } from '@/auth';
-import Image from 'next/image';
 import UserMenu from './UserMenu';
 import SignIn from './ui/signIn';
-import Logo from '@/public/social-paroi.png';
+import Drawer from './Drawer';
 
 export default async function NavBar() {
   const session = await auth();
-  if (session?.user) {
-    session.user = {
-      id: session.user.id,
-      name: session.user.name,
-      email: session.user.email,
-      image: session.user.image,
-    }
-  }
 
   return (
     <SessionProvider session={session}>
-      <nav className="sticky flex justify-center border-b border-gray-700">
-        <div className="flex items-center justify-between w-full h-16 max-w-3xl px-4 mx-auto sm:px-6">
-          <Image src={Logo} alt="Social Paroi app logo" width={40} height={40} />
+      <nav className="sticky z-20 flex justify-center border-b border-gray-400 dark:border-gray-700">
+        <div className="flex items-center justify-between w-full h-16 px-4 mx-auto sm:px-6">
+          <Drawer />
           <div className="text-4xl md:text-5xl font-extrabold">
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
               Social Paroi
