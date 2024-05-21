92ba937941e20b7c0f5b2096e43f4718c8bdc929
Jérémie <jeremie.faussion@gmail.com>
2024-05-21 08:38:05 +0200

    [TEST] add use effect and logging

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -2,7 +2,7 @@
 
 import { isOpener } from '@/utils/session.utils';
 import { useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IoClose } from 'react-icons/io5';
 import { LuMenu } from 'react-icons/lu';
 import Image from 'next/image';
@@ -19,6 +19,13 @@ const Drawer = () => {
   const router = useRouter();
 
   const showBackButtonInsteadOfMenu = () => pathname.startsWith('/dashboard/track/');
+
+  useEffect(() => {
+    if (session.status === 'authenticated') {
+      console.log('Session Data:', session.data);
+      console.log('Is Opener:', isOpener(session.data));
+    }
+  }, [session]);
 
   return (
     <div>
@@ -55,7 +62,7 @@ const Drawer = () => {
             <li>
               <Link className="block cursor-pointer hover:bg-gray-300 hover:dark:bg-gray-700 p-2 rounded-md" href="/dashboard">Dashboard</Link>
             </li>
-            {isOpener(session.data) && (
+            {session.status === 'authenticated' && isOpener(session.data) && (
               <>
                 <hr className="border-t border-gray-400 dark:border-gray-600" />
                 <li>
