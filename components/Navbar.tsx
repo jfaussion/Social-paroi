9cbfadba3e5e7d70f15bf41d3d7899ea5c102f20
Jérémie <64860341+jfaussion@users.noreply.github.com>
2024-05-21 08:58:32 +0200

    Feature/drawer menu (#29) (#33)
    * Init drawer menu
    
    * [FIX] drawer z-index
    
    * Refactor Drawer component to improve menu navigation and add back button support
    
    * Align close button to top corner right
    
    * Add drawer footer with utility links
    
    * feat: add footer links to Drawer component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,14 +7,6 @@ import Drawer from './Drawer';
 
 export default async function NavBar() {
   const session = await auth();
-  if (session?.user) {
-    session.user = {
-      id: session.user.id,
-      name: session.user.name,
-      email: session.user.email,
-      image: session.user.image,
-    }
-  }
 
   return (
     <SessionProvider session={session}>
