e8fdd9c992580872e9aa3751e3195d9f2dd01c41
Jérémie <jeremie.faussion@gmail.com>
2024-06-15 16:32:32 +0200

    chore: Update RankingList component to handle error and loading states

--- components/RankingList.tsx
+++ components/RankingList.tsx
@@ -3,11 +3,12 @@ import { useSession } from "next-auth/react";
 import UserRanking from "./UserRanking";
 import { useFetchRanking } from "@/lib/useFetchRanking";
 import { useEffect, useState } from "react";
+import { RankingPlaceholder } from "./ui/RankingPlaceholder";
 
 
-const RankingList: React.FC<any> = ({ initialTrack }) => {
+const RankingList: React.FC = () => {
   const session = useSession();
-  const {fetchRanking, isLoading, error} = useFetchRanking();
+  const { fetchRanking, isLoading, error } = useFetchRanking();
   const [ranking, setRanking] = useState<any[]>([]);
 
   useEffect(() => {
@@ -18,29 +19,44 @@ const RankingList: React.FC<any> = ({ initialTrack }) => {
     getRanking();
   }, []);
 
-
   return (
-    <div className='w-full border border-gray-600 rounded-lg bg-gray-100 dark:bg-gray-900 sm:m-4 sm:mt-0 space-y-2'>
-      <div className="flex flex-col sm:p-4">
-        <span className="text-md p-4 w-full font-medium">Based on mounted blocs only</span>
-        <div className="flex justify-between mb-3">
-          <span className="text-sm font-medium"></span>
-          <div className="flex justify-self-end space-x-2">
-            <span className="text-sm w-[4rem] sm:w-[8rem] text-center font-semibold">Score</span>
+    <>
+      {error && (
+        <div className='w-full border border-gray-600 rounded-lg bg-gray-100 dark:bg-gray-900 sm:m-4 sm:mt-0 space-y-2'>
+          <div className="flex p-4 sm:p-8">
+            An error occurred while fetching the ranking
           </div>
         </div>
-        {ranking.map((user, index) => (
-          <UserRanking
-            key={user.id}
-            rank={index + 1}
-            profilePicture={user.image}
-            name={user.name}
-            score={user.score}
-            isCurrentUser={user.id === session.data?.user?.id}
-          />
-        ))}
-      </div>
-    </div>
+      )}
+      {isLoading && (
+        <div className='h-full w-full sm:m-4 sm:mt-0 space-y-2'>
+          <RankingPlaceholder />
+        </div>
+      )}
+      {!isLoading && ranking?.length > 0 && (
+        <div className='w-full border border-gray-600 rounded-lg bg-gray-100 dark:bg-gray-900 sm:m-4 sm:mt-0 space-y-2'>
+          <div className="flex flex-col sm:p-4">
+            <span className="text-md p-4 w-full font-medium">Based on mounted blocs only</span>
+            <div className="flex justify-between mb-3">
+              <span className="text-sm font-medium"></span>
+              <div className="flex justify-self-end pr-4 space-x-2">
+                <span className="text-lg w-[4rem] sm:w-[8rem] text-end font-semibold">Score</span>
+              </div>
+            </div>
+            {ranking.map((user, index) => (
+              <UserRanking
+                key={user.id}
+                rank={index + 1}
+                profilePicture={user.image}
+                name={user.name}
+                score={user.score}
+                isCurrentUser={user.id === session.data?.user?.id}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
