f9091d15100615d306b0b2b3f9ae11ca5644d724
Jérémie <jeremie.faussion@gmail.com>
2024-08-23 10:02:55 +0200

    doc + clean code

--- lib/news/hooks/usePostNews.ts
+++ lib/news/hooks/usePostNews.ts
@@ -2,10 +2,19 @@ import { useState } from 'react';
 import { News } from '@/domain/News.schema';
 import { postNews } from '../actions/postNews';
 
+/**
+ * Custom hook for posting news.
+ * @returns An object containing the function to post the news (postNewsDate), the loading state (isLoading), the error message (error).
+ */
 export const usePostNews = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
+  /**
+   * Posts news data.
+   * @param news - The news object to be posted.
+   * @returns A promise that resolves to the posted news object, or null if an error occurred.
+   */
   const postNewsData = async (news: News): Promise<News | null> => {
     setIsLoading(true);
     setError(null);
