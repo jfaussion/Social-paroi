eaca35807e9075a347ccf84dde8e1312980c8963
Jérémie <jeremie.faussion@gmail.com>
2025-01-06 17:37:29 +0100

    make activity description non null

--- domain/ContestActivity.schema.ts
+++ domain/ContestActivity.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const ContestActivitySchema = z.object({
   id: z.number(),
   name: z.string().min(1, "Name is required"),
-  description: z.string().nullish(),
+  description: z.string(),
   image: z.string().nullish(),
   contestId: z.number(),
 });
