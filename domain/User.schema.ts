d4155a898fcd4c2c4eb77a4fb3807e1279e35c22
Jérémie <jeremie.faussion@gmail.com>
2025-01-06 22:52:31 +0100

    Enhance UserSchema to include optional contestUsers field
    - Added an optional contestUsers array to the UserSchema, allowing for the inclusion of temporary contest participants with associated contest details (id, name, date).
    - This change improves the schema's flexibility in handling user contest participation data.

--- domain/User.schema.ts
+++ domain/User.schema.ts
@@ -7,7 +7,14 @@ export const UserSchema = z.object({
   image: z.string().nullish(),
   emailVerified: z.date().nullable(),
   role: z.string().nullish(),
-
+  contestUsers: z.array(z.object({
+    isTemp: z.boolean(),
+    contest: z.object({
+      id: z.number(),
+      name: z.string(),
+      date: z.date()
+    })
+  })).optional()
 });
 
 export type User = z.infer<typeof UserSchema>;
